352ac14cc81e4f7d1602a77abfec8fd3d4c34e92
Davushruthi <shruthidavu75108gmail.com>
2026-08-31 19:58:19 +0530

    Task 4

---
+++ Helloworld/src/TASK4/mainclass.java
@@ -0,0 +1,16 @@
+package TASK4;
+
+public class mainclass {
+
+	public static void main(String[] args) {
+		conditionalprgm b= new  conditionalprgm();
+		b.basic();
+		b.leapyear();
+		loops obj = new loops();
+		obj.evensum();
+		obj.fact();
+		}
+
+	}
+
+
